6844ef843718554f73b441e78c63f2e8807fb33e
Nicowski25 <nicola.faedo0610@gmail.com>
2023-03-15 15:33:44 +0100

    shopping list and get dom ul

---
+++ assets/js/script.js
@@ -0,0 +1,18 @@
+//creiamo la shopping list
+const shoppingList = [
+    'mele',
+    'pere',
+    'arance',
+    'limoni'
+]
+
+//prendiamo il container dalla dom
+const lista = document.getElementsByTagName('ul')
+console.log(lista);
+
+//creiamo un ciclo while col quale aggiungiamo i list item
+let i = 0
+while (i < shoppingList.length) {
+    
+}
+
